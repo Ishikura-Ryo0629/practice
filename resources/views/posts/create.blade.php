bbe253baeba11571957b87518acc2beed047dfe8
Ishikura-Ryo0629 <ishikura27@gmail.com>
2022-08-28 21:54:34 +0900

    ブログ投稿作成の保存処理

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,7 +7,7 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <form action="/posts" "method"=POST>
+        <form action="/posts" "method"=post>
             @csrf
             <div class="title">
                 <h2>Title</h2>
