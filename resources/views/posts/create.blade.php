53ac7fe025d40c5b54cebfed57db64889ed8b36c
Ishikura-Ryo0629 <ishikura27@gmail.com>
2022-08-28 22:58:24 +0900

    エラーメッセージの表示と入力情報の保持

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,14 +11,17 @@
             @csrf
             <div class="title">
                 <h2>Title</h2>
-                <input type="text" name="post[title]" placeholder="タイトル"/>
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="body">
                 <h3>Body</h3>
-                <textarea name="post[body]" placeholder="今日も一日お疲れさまでした。"></textarea>
+                <textarea name="post[body]" placeholder="今日も一日お疲れさまでした。" value="{{ old('post.body') }}"></textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             <input type="submit" value="保存"/>
         </form>
+        
         <div class="back">[<a href="/">back</a>]</div>
     </body>
 </html>
