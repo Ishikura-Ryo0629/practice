bafae98c64bce993c107654acc8e6b39f7d5d435
Ishikura-Ryo0629 <ishikura27@gmail.com>
2022-08-28 23:47:30 +0900

    編集画面の追加

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -8,20 +8,20 @@
     </head>
     <body>
         <h1 class="title">編集画面</h1>
-        <form action="/posts/{{ $post->id }}" "method"=POST>
+        <div class="content">
+        <form action="/posts/{{ $post->id }}" method="POST">
             @csrf
             @method('PUT')
-            <div class="content__title">
+            <div class='content__title'>
                 <h2>タイトル</h2>
-                <input type="text" name="post[title]" placeholder="タイトル" value="{{ $post.title }}"/>
+                <input type='text' name='post[title]' value="{{ $post->title }}">
             </div>
-            <div class="body">
-                <h3>Body</h3>
-                <textarea name="post[body]" placeholder="今日も一日お疲れさまでした。" value="{{ $post.body }}"></textarea>
+            <div class='content__body'>
+                <h2>本文</h2>
+                <input type='text' name='post[body]' value="{{ $post->body }}">
             </div>
-            <input type="submit" value="保存"/>
+            <input type="submit" value="保存">
         </form>
-        
-        <div class="back">[<a href="/">back</a>]</div>
+        </div>
     </body>
 </html>
