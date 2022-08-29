307fdb0a9c463f875f7d2d89970ad5850d328107
Ishikura-Ryo0629 <ishikura27@gmail.com>
2022-08-29 15:20:16 +0900

    投稿削除機能の実装

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,12 +13,29 @@
                 <div class="content_post">
                     <h3>本文</h3>
                     <p>{{ $post->body }}</p>
+                    <p class="updated_at">{{ $post->updated_at }}</p>
                 </div>
             </div>
         </div>
         <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+        <form action="/posts/{{ $post->id }}" id="form_delete" method="post" style="display:inline">
+            @csrf
+            @method('DELETE')
+            <!--<input type="button" style="display:none"/>-->
+            <!--<p class="delete" onclick="return deletePost()";>[<span>削除</span>]</p>-->
+            <button onclick="return deletePost()";>削除</button>
+        </form>
         <div class="footer">
             <a href="/">back</a>
         </div>
+        <script>
+            function deletePost() {
+                const confirm = window.confirm('本当に削除しますか？')
+                if (confirm)  {
+                    return true;
+                    }
+                return false;
+            }
+        </script>
     </body>
 </html>
